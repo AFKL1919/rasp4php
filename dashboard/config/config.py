8c174a1f4b1d8d002929ac37a3956b20ffb6bad3
AFKL-CUIT <afkl1919@qq.com>
2023-03-05 17:59:54 +0800

    Add the ability to load custom configurations.

--- dashboard/config/config.py
+++ dashboard/config/config.py
@@ -1,13 +1,20 @@
-from rasp.common.config import PROJECT_ROOT
 from dashboard.core.webapp import WEBAPP
+
 import json
+from os import urandom
+from datetime import timedelta
+
 try:
     from pathlib import Path
 except ImportError:
     from pathlib2 import Path
 
+PROJECT_ROOT = Path(__file__).parent.parent.parent
 config_file = PROJECT_ROOT / "resources" / "dashboard" / "config.json"
 config_file_data = config_file.read_text()
 
 CONFIG_DATA = json.loads(config_file_data)
-WEBAPP.config["SQLALCHEMY_DATABASE_URI"] = CONFIG_DATA["WEBAPP"]["SQLALCHEMY_DATABASE_URI"]
+
+WEBAPP.config["SECRET_KEY"] = urandom(32) if CONFIG_DATA["APP_CONFIG"]["SECRET_KEY"] == "" else CONFIG_DATA["APP_CONFIG"]["SECRET_KEY"]
+WEBAPP.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=CONFIG_DATA["APP_CONFIG"]["PERMANENT_SESSION_LIFETIME"])
+WEBAPP.config["SQLALCHEMY_DATABASE_URI"] = CONFIG_DATA["DB"]["SQLALCHEMY_DATABASE_URI"]
--- rasp/common/config.py
+++ rasp/common/config.py
@@ -1,14 +1,12 @@
 from __future__ import unicode_literals
+import json
+
 try:
     from pathlib import Path
 except ImportError:
     from pathlib2 import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
-config_file = PROJECT_ROOT / "resources" / "dashboard" / "config.json"
-
-__VERSION__ = '0.6.1'
-APPLICATION_NAME = "RASP4PHP"
 
 RASP_APP_ROOT = PROJECT_ROOT / "rasp"
 DASHBOARD_ROOT = PROJECT_ROOT / "dashboard"
@@ -16,3 +14,11 @@ RESOURCES_ROOT = PROJECT_ROOT / "resources"
 
 RASP_APP_RESOURCES = RESOURCES_ROOT / "rasp"
 DASHBOARD_RESOURCES = RESOURCES_ROOT / "dashboard"
+
+config_file = PROJECT_ROOT / "resources" / "rasp" / "config.json"
+config_file_data = config_file.read_text()
+
+CONFIG_DATA = json.loads(config_file_data)
+
+__VERSION__ = CONFIG_DATA["RASP_CONFIG"]["VERSION"]
+APPLICATION_NAME = CONFIG_DATA["RASP_CONFIG"]["APPLICATION_NAME"]
