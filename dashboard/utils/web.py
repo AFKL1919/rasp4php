4f7f2b21939126983f7126cefccbdec3ff382078
AFKL-CUIT <afkl1919@qq.com>
2023-03-05 21:14:33 +0800

    Modified session related content in `web.py`.

--- dashboard/utils/web.py
+++ dashboard/utils/web.py
@@ -1,14 +1,16 @@
 from rasp.core.app import RASP_APP
+from dashboard.core.webapp import WEBAPP
 from dashboard.models.users import Users
 
 from functools import wraps
 from flask import jsonify, redirect, url_for, request, session, abort
 
 def json_data(data, status_code: int = 200):
-    return jsonify({
+    res_data = {
         "status": status_code,
         "data": data
-    })
+    }
+    return jsonify(res_data)
 
 def not_install_required(f):
     @wraps(f)
@@ -22,23 +24,28 @@ def not_install_required(f):
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        user = session["user_data"]
-        if user is None:
+        if "user_data" not in session:
             return redirect(url_for('login', next=request.url))
         
-        return f(*args, **kwargs)
+        user = session["user_data"]
+        if isinstance(user, dict) and user["username"] != "":
+            return f(*args, **kwargs)
+        
+        return redirect(url_for('login', next=request.url))
+        
     return decorated_function
 
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        if "user_data" not in session:
+            return redirect(url_for('login', next=request.url))
+        
         user = session["user_data"]
-        
-        if user is None:
-            return abort(403)
-        
-        user = Users(user)
-        if user.permission != Users.ADMIN_USER:
+        if (
+            not isinstance(user, dict) and 
+            user["permission"] != Users.ADMIN_USER
+        ):
             return abort(403)
         
         return f(*args, **kwargs)
