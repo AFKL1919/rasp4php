b5210b73ef11a7ad899ede92760e7cfb1027bc6f
AFKL-CUIT <afkl1919@qq.com>
2023-03-20 17:01:15 +0800

    🎈 perf(route): 添加route
    添加rule操作的route

--- dashboard/controller/api/__init__.py
+++ dashboard/controller/api/__init__.py
@@ -2,3 +2,4 @@ import dashboard.controller.api.hook
 import dashboard.controller.api.install
 import dashboard.controller.api.log
 import dashboard.controller.api.user
+import dashboard.controller.api.rule
