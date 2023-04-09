2fba5387b76788f56a9aaa2da6e22d6f13fb285f
AFKL-CUIT <afkl1919@qq.com>
2023-04-10 03:03:07 +0800

    🐞 fix(hook): 更新ban ip相关hook
    将`zend_execute_scripts`的hook脚本由`Interceptor.replace`改为`Interceptor.attach`

--- rasp/hooks/v7/zend_execute_scripts.js
+++ rasp/hooks/v7/zend_execute_scripts.js
@@ -1,36 +1,36 @@
-const FAIL_VALUE = -1;
 var origin_ptr = Module.findExportByName(null, 'zend_execute_scripts');
-var origin_func = new NativeFunction(origin_ptr, 'int', ['int','pointer','int']);
-Interceptor.replace(origin_ptr, new NativeCallback(
-  (...args) => {
-    var message = {
-      pid: Process.id,
-      function: getFunctionName(),
-      args: [],
-      normalized_args: [],
-      filename: getFilename(),
-      lineno: getLineNo(),
-      context: 'start_request',
-      type: 'start_request',
-      request_uri: getServerEnv('REQUEST_URI'),
-      remote_addr: getServerEnv('REMOTE_ADDR'),
-      query_string: getServerEnv('QUERY_STRING'),
-      document_root: getServerEnv('DOCUMENT_ROOT'),
-      hook_point: 'zend_execute_scripts'
-    };
+Interceptor.attach(
+  Module.findExportByName(null, 'zend_execute_scripts'),
+  {
+    onEnter(args) {
+      var message = {
+        pid: Process.id,
+        function: getFunctionName(),
+        args: [],
+        normalized_args: [],
+        filename: getFilename(),
+        lineno: getLineNo(),
+        context: 'start_request',
+        type: 'start_request',
+        request_uri: getServerEnv('REQUEST_URI'),
+        remote_addr: getServerEnv('REMOTE_ADDR'),
+        query_string: getServerEnv('QUERY_STRING'),
+        document_root: getServerEnv('DOCUMENT_ROOT'),
+        hook_point: 'zend_execute_scripts'
+      };
 
-    send(message);
-    var judge_msg = null;
-    recv(message => {
-      judge_msg = message;
-    }).wait();
+      send(message);
+      var judge_msg = null;
+      recv(message => {
+        judge_msg = message;
+      }).wait();
 
-    if (judge_msg['is_blocked']) {
-      block_request_for_blacklist_ip(judge_msg['code'], judge_msg['body'], judge_msg['headers']);
-      return origin_func(args[0], args[1], 0);
+      if (judge_msg['is_blocked']) {
+        block_request_for_blacklist_ip(
+          judge_msg['code'], judge_msg['body'], judge_msg['headers']
+        );
+        args[2].writeInt(0);
+      }
     }
-
-    return origin_func(...args);
-  }, 'int', ['int','pointer','int']
-)
+  }
 );
