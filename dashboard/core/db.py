d8f7752999972df26b531481967262769efd054b
AFKL-CUIT <afkl1919@qq.com>
2023-04-10 03:04:34 +0800

    🐎 ci(DB): 更换为pgsql
    将rasp的数据库由sqlite更换为pgsql

--- dashboard/core/db.py
+++ dashboard/core/db.py
@@ -1,9 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
-
 from rasp.utils.log import logger
 from dashboard.core.webapp import WEBAPP
 
-DB_BASE = SQLAlchemy(WEBAPP, session_options={"expire_on_commit": False})
+from time import sleep
+from flask_sqlalchemy import SQLAlchemy
+
+DB_BASE = SQLAlchemy(
+    WEBAPP, 
+    session_options={
+        "expire_on_commit": False
+    }
+)
+
+MAX_RETRIES = 6
+
+for i in range(MAX_RETRIES):
+    try:
+        with WEBAPP.app_context():
+            DB_BASE.engine.connect()
+    except Exception as ex:
+        logger.error(f"Failed to connect. Try again: {ex}")
+        sleep(2)
+    else:
+        break
+else:
+    raise Exception("Failed to connect to database after maximum retries")
+
 DB_SESSION = DB_BASE.session
 DB_BASE.init_app(WEBAPP)
 
