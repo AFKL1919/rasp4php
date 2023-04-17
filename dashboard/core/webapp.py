2b0a90666463271ef5afa1a4922d7340779e05de
AFKL-CUIT <afkl1919@qq.com>
2023-04-17 22:11:30 +0800

    🐞 fix(web-core): 添加模板路径
    添加模板路径

--- dashboard/core/webapp.py
+++ dashboard/core/webapp.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from rasp.utils.log import logger
 from rasp.common.config import APPLICATION_NAME, DASHBOARD_RESOURCES
 
-WEBAPP = Flask(APPLICATION_NAME, template_folder=DASHBOARD_RESOURCES / "template")
+WEBAPP = Flask(
+    APPLICATION_NAME, 
+    template_folder=DASHBOARD_RESOURCES / "template"
+)
 
 def web_app_start(host: str, port: int):
     import dashboard.controller
