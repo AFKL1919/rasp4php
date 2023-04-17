cc1ee3cc740cb5915a5df2157fe28723acc22e04
AFKL-CUIT <afkl1919@qq.com>
2023-04-17 22:10:03 +0800

    ✨ feat(rule): 添加规则的下载
    添加规则的下载

--- dashboard/controller/api/rule.py
+++ dashboard/controller/api/rule.py
@@ -4,8 +4,9 @@ from dashboard.utils.web import json_data, admin_required, login_required
 from rasp.core.rule import RULE_MANAGER
 from rasp.core.filter import FILTER_MANAGER
 from rasp.utils.log import logger
+from rasp.common.config import Path
 
-from flask import request
+from flask import request, send_file
 
 
 @WEBAPP.route("/api/rules", methods=["GET"])
@@ -37,7 +38,7 @@ def get_rule_with_param():
 
     rule_obj_list = RULE_MANAGER.get_rule_list(classname)
 
-    if rule_obj_list == list():
+    if not rule_obj_list:
         return json_data("该过滤器不存在", 400)
 
     rule_list = [rule.serialize() for rule in rule_obj_list]
@@ -47,13 +48,11 @@ def get_rule_with_param():
             lambda rule: "rule_type" in rule and rule["rule_type"] == rule_type, rule_list
         ))
 
-    return json_data({
-        classname: rule_list
-    })
+    return json_data(rule_list)
 
 @WEBAPP.route("/api/rule/update", methods=["POST"])
 @admin_required
-def update_rule():
+def update_rule_by_params():
     classname = request.form.get("classname")
     rule_id = request.form.get("rule_id", type=int)
 
@@ -86,3 +85,32 @@ def update_rule():
     logger.info(f"filter rules changed Filter:{classname} rules:{[rule.serialize() for rule in RULE_MANAGER.get_rule_list(classname)]}")
     return json_data("修改成功")
 
+
+@WEBAPP.route("/api/rule/dump", methods=["POST"])
+@login_required
+def dump_rule_with_param():
+    classname = request.form.get("classname")
+    
+    try:
+        path = RULE_MANAGER.dump_rules_to_file(classname)
+    except Exception as e:
+        return json_data({"msg":"error"}, 500)
+
+    return json_data(f"成功导出至：{str(path)}")
+
+
+@WEBAPP.route("/api/rule/download", methods=["POST"])
+@login_required
+def download_rule_with_param():
+    classname = request.form.get("classname")
+    rule_obj_list = RULE_MANAGER.get_rule_list(classname)
+
+    if not rule_obj_list:
+        return json_data("该过滤器不存在", 400)
+    
+    file = Path(rule_obj_list[0].filename)
+    if not file.is_file():
+        logger.error(f"导出规则文件失败，文件名：{str(file)}")
+        return json_data("下载失败", 500)
+    
+    return send_file(str(file))
