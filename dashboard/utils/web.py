04f993733198463e01ae64d9c1d65637d5ef26b4
AFKL-CUIT <afkl1919@qq.com>
2023-04-17 22:12:41 +0800

    ✨ feat(utils): 添加分页
    添加分页函数

--- dashboard/utils/web.py
+++ dashboard/utils/web.py
@@ -12,6 +12,20 @@ def json_data(data, status_code: int = 200):
     }
     return jsonify(res_data)
 
+def get_page_and_size(get_data: dict):
+    page = get_data.get('page', 1)
+    if not isinstance(page, int):
+        page = 1
+    size = get_data.get('size', 10)
+    if not isinstance(size, int):
+        size = 10
+    return int(page), int(size)
+
+def get_pagination(page, size):
+    start = (page - 1) * size
+    end = start + size
+    return start, end
+
 def not_install_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -25,13 +39,13 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "user_data" not in session:
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('login_page', next=request.url))
         
         user = session["user_data"]
         if isinstance(user, dict) and user["username"] != "":
             return f(*args, **kwargs)
         
-        return redirect(url_for('login', next=request.url))
+        return redirect(url_for('login_page', next=request.url))
         
     return decorated_function
 
@@ -39,7 +53,7 @@ def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "user_data" not in session:
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('login_page', next=request.url))
         
         user = session["user_data"]
         if (
