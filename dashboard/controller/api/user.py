78fc64f87c089a4cf007cff486b1d53c388992fb
AFKL-CUIT <afkl1919@qq.com>
2023-04-17 22:10:42 +0800

    🐞 fix(user): 修复更新用户的类型问题
    修复更新用户的类型问题

--- dashboard/controller/api/user.py
+++ dashboard/controller/api/user.py
@@ -9,20 +9,24 @@ from flask import session, request
 @WEBAPP.route("/api/user/update", methods=["POST"])
 @admin_required
 def update_info():
-    username = request.form.get("username")
-    new_password = request.form.get("new_password")
+    user_id = request.form.get("user_id", type=str)
+    username = request.form.get("username", type=str)
+    new_password = request.form.get("new_password", type=str)
+    permission = request.form.get("permission", type=str)
 
-    if isinstance(new_password, str) and isinstance(username, str):
+    if new_password and username:
         user = Users.query.filter(
-            Users.username == username
+            Users.id == int(user_id)
         ).update({
-            'password': new_password
+            'username': username,
+            'password': new_password,
+            'permission': permission
         })
         DB_SESSION.commit()
     else:
         return json_data("输入格式错误", 400)
 
-    if user.id == None:
+    if not isinstance(user, int):
         return json_data("用户信息修改失败", 500)
 
     return json_data("用户信息修改成功", 200)
@@ -31,10 +35,10 @@ def update_info():
 @WEBAPP.route("/api/user/admin_register", methods=["POST"])
 @not_install_required
 def register_admin():
-    username = request.form.get("username")
-    password = request.form.get("password")
+    username = request.form.get("username", type=str)
+    password = request.form.get("password", type=str)
 
-    if isinstance(password, str) and isinstance(username, str):
+    if password and username:
         user = Users(username, password, Users.ADMIN_USER)
     else:
         return json_data("输入格式错误", 400)
@@ -89,7 +93,7 @@ def login():
         Users.password == password
     ).first()
 
-    if not isinstance(user, Users):
+    if user.id == None:
         return json_data("登录失败", 401)
 
     session["user_data"] = user.serialize()
